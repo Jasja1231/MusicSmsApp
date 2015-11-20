ccf0ac680c6fecfb3e7193d52c20b149ef7badb9
Yaryna <jasja1231@gmail.com>
2015-11-20 03:25:22 +0000

    Initial commit

---
+++ app/src/main/java/com/example/yaryna/musicsmsapp/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.yaryna.musicsmsapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.telephony.SmsMessage;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        //creating a receiver for SMS messages
+        BroadcastReceiver myReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Bundle bundle = intent.getExtras();
+                Object[] pdus = (Object[])bundle.get("pdus");
+                for(Object pduObject:pdus){
+                    byte[] pdu = (byte[]) pduObject;
+                    SmsMessage message = SmsMessage.createFromPdu(pdu);
+                    processMessage(message);
+                }
+            }
+        };
+
+        //Filter for intent : receive message
+        IntentFilter filter = new IntentFilter(SMS_RECEIVED);
+        registerReceiver(myReceiver,filter);
+    }
+
+    /**
+     * Method to process received SMS messages
+     * @param message SmsMessage object constructed from received SMS
+     **/
+    private void processMessage(SmsMessage message){
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
